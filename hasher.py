d188930b9a6e7af4c1384d176109fd54039cd991
toa <127238329+6at4re9i@users.noreply.github.com>
2023-05-02 01:19:53 +0500

    Add files via upload

---
+++ hasher.py
@@ -0,0 +1,11 @@
+import hashlib
+
+data = input("Enter the string to hash: ")
+encoded_data = data.encode('utf-8')
+hash_object = hashlib.md5(encoded_data.strip())
+hash_value = hash_object.hexdigest()
+print(hash_value)
+
+
+
+
