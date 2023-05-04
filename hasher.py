71b394b9483eb03aa3226a2d0dbaa6cd06a82ca1
toa <127238329+6at4re9i@users.noreply.github.com>
2023-05-04 17:31:56 +0500

    Update hasher.py

--- hasher.py
+++ hasher.py
@@ -6,6 +6,7 @@ hash_object = hashlib.md5(encoded_data.strip())
 hash_value = hash_object.hexdigest()
 print(hash_value)
 
-# you can hash whatever you want!
+# you can hash whatever you want by protocol md5
+!
 
 
